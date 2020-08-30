8481f166b51190ef7cf4dc6a034aabd69c75ea8d
Muhammad Essam <muhammadessamwork@outlook.com>
2020-08-30 05:08:59 +0200

    adds subscription to users show

--- resources/views/admin/clients/partials/_orders.blade.php
+++ resources/views/admin/clients/partials/_orders.blade.php
@@ -7,6 +7,7 @@
                 <th>رقم الطلب</th>
                 <th>اسم العميل</th>
                 <th>العنوان</th>
+                <th>الاشتراك</th>
                 <th>المبلغ</th>
                 <th>الحالة</th>
                 <th>حالة الدفع</th>
@@ -22,6 +23,7 @@
                     <td>{{$item->branch->bill_prefix}}-{{$item['serial']}}</td>
                     <td>{{$item->client->name}}</td>
                     <td>{{$item->address ? $item->address->name : ''}} - {{$item->address && $item->address->city ?$item->address->city->name:''}} -{{$item->address && $item->address->area ?$item->address->area->name:''}}  </td>
+                    <td>{{$item->subscription ? $item->subscription->name:''}}</td>
                     <td>{{$item->total}}</td>
                     <td>{{$item->status ? $item->status->name : ''}}</td>
                     <td>
